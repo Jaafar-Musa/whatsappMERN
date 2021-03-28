97151db15de017cf22e6f4639146d7d65b5898c3
Jaafar-Musa02 <jaafarmusa.jm02@gmail.com>
2021-03-28 01:53:29 +0000

    backend

--- whatsapp-frontend/src/App.js
+++ whatsapp-frontend/src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 
+// function get(){
+//   fetch('http://localhost:9000/').then(
+//     response => console.log(response.json())
+//   )
+// }
+
 
 function App() {
+  // { get() }
   return (
     <div className="app">
       <section className="app__body">
